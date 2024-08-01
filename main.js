50598f49fcc663c3817641bf509dcd5794a11ef1
SofiSuriano <L23270622@tuxtla.tecnm.mx>
2024-07-31 21:40:31 -0600

    Cree el main

---
+++ main.js
@@ -0,0 +1,18 @@
+//console.log("Hola Mundo! desde la consola");
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString(' Ing. en sistemas computacionales|Desarrolladora Frontend Jr.')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString(' ¡Si lo puedes imaginar, lo puedes crear!')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .typeString('<strong>programar!!!</strong>')
+    .pauseFor(2500)
+    .start();
+
